ac73288173e696e1722a33bdc0565cb6ada1e7d0
jcirni <josephcirnigliaro@gmail.com>
2016-04-12 01:16:07 -0400

    Add key press feature

--- app.js
+++ app.js
@@ -2,9 +2,24 @@ var main = function () {
     "use strict";
     
     $(".comment-input button").on("click", function (event) {
-        var $new_comment = $("<p>").text($(".comment-input input").val());
+        var $new_comment;
         
-        $(".comments").append($new_comment);
+        if ($(".comment-input input").val() !== "") {
+            $new_comment = $("<p>").text($(".comment-input input").val());
+            $(".comments").append($new_comment);
+            $(".comment-input input").val("");
+        }
+     });
+    
+    $(".comment-input input").on("keypress", function (event) {
+        var $new_comment;
+        
+        if (event.keyCode === 13) {
+            $new_comment = $("<p>").text($(".comment-input input").val());
+            $(".comments").append($new_comment);
+            $(".comment-input input").val("");
+        }
+            
     });
 };
 
