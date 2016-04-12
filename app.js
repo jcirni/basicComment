86d66bbd7f4d5a506b3de6adf90e7c792403f526
jcirni <josephcirnigliaro@gmail.com>
2016-04-12 01:28:24 -0400

    optimize app.js and add basic fades to comment sections

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
 var main = function () {
     "use strict";
     
-    $(".comment-input button").on("click", function (event) {
+    var addCommentFromInput = function () {
         var $new_comment;
         
         if ($(".comment-input input").val() !== "") {
             $new_comment = $("<p>").text($(".comment-input input").val());
+            $new_comment.hide();
             $(".comments").append($new_comment);
+            $new_comment.fadeIn();
             $(".comment-input input").val("");
         }
-     });
+    };
+    
+    $(".comment-input button").on("click", function (event) {
+        addCommentFromInput();
+    });
     
     $(".comment-input input").on("keypress", function (event) {
-        var $new_comment;
-        
+
         if (event.keyCode === 13) {
-            $new_comment = $("<p>").text($(".comment-input input").val());
-            $(".comments").append($new_comment);
-            $(".comment-input input").val("");
+            addCommentFromInput();
         }
             
     });
